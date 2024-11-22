55a51d36f2d9683180e73133ed1fbf6ecdef03e7
TarekElqari <telqari@gmail.com>
2024-11-22 20:59:54 +0100

    Delete RestController and add new project files

---
+++ gestion-etudiant-tp5/src/main/java/ma/emsi/gestionetudianttp5/GestionEtudiantTp5Application.java
@@ -0,0 +1,13 @@
+package ma.emsi.gestionetudianttp5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionEtudiantTp5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionEtudiantTp5Application.class, args);
+    }
+
+}
---
+++ gestion-etudiant-tp5/src/test/java/ma/emsi/gestionetudianttp5/GestionEtudiantTp5ApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.emsi.gestionetudianttp5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionEtudiantTp5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
